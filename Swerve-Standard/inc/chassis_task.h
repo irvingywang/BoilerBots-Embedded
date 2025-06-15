5da9ded18675b92a59eee76b4bb6b57c6c6857a5
andyNiu <aniu2240@gmail.com>
2025-06-15 14:05:16 -0400

    fixed typo

--- Swerve-Standard/inc/chassis_task.h
+++ Swerve-Standard/inc/chassis_task.h
@@ -5,7 +5,7 @@
 #define SWERVE_MAX_SPEED 1.4f          // m/s
 #define SPIN_TOP_OMEGA 4.0f            // m/s
 #define SWERVE_MAX_ANGLUAR_SPEED 3.14f // rad/s
-#define TRACK_WIDTH 0.34f              // m, measu\red wheel to wheel (side to side)
+#define TRACK_WIDTH 0.34f              // m, measured wheel to wheel (side to side)
 #define WHEEL_BASE 0.34f               // m, measured wheel to wheel (up and down)
 #define WHEEL_DIAMETER 0.12f           // m, measured wheel diameter
 #define SWERVE_MAX_WHEEL_ACCEL 0.8f    // m/s^2
